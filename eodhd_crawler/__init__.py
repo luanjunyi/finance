5a2d25b5d90b4f1ba0d31805429a94dc28990351
Jerry Luan <xmaswillyou@gmail.com>
2025-07-23 06:43:35 -0700

    [infra] add EODHD price cralwer

---
+++ eodhd_crawler/__init__.py
@@ -0,0 +1,8 @@
+"""
+EODHD Crawler Package
+
+This package contains crawlers for fetching financial data from EODHD API.
+"""
+
+# Import only when needed to avoid conflicts when running modules as scripts
+__all__ = ['EODHDBatchPriceCrawler']
