47b4e47799c8308f3d3a9b77916c06d6d4ebf0a5
Jerry Luan <xmaswillyou@gmail.com>
2025-08-21 17:42:36 -0700

    [infra] crawler and query function for spx constituent in history

--- fmp_crawl.py
+++ fmp_crawl.py
@@ -5,10 +5,11 @@ from fmp_crawler.symbol_crawler import SymbolCrawler
 from fmp_crawler.price_crawler import PriceCrawler
 from fmp_crawler.financial_crawler import FinancialCrawler
 from fmp_crawler.metrics_crawler import MetricsCrawler
+from fmp_crawler.spx_constituent_crawler import SpxConstituentCrawler
 
 async def main():
     parser = argparse.ArgumentParser(description='FMP Data Crawler')
-    parser.add_argument('--type', choices=['symbol', 'price', 'financial', 'metrics'],
+    parser.add_argument('--type', choices=['symbol', 'price', 'financial', 'metrics', 'spx_constituent'],
                         required=True, help='Type of data to crawl')
     parser.add_argument('--db-path', required=True, help='Path to the database file taht store the crawled result')
 
@@ -23,6 +24,8 @@ async def main():
         crawler = FinancialCrawler(db_path)
     elif args.type == 'metrics':
         crawler = MetricsCrawler(db_path)
+    elif args.type == 'spx_constituent':
+        crawler = SpxConstituentCrawler(db_path)
     else:
         raise ValueError(f"Invalid type: {args.type}")
 
